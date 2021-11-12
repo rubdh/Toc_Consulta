72a0d01b2b6757b43771eed79fd9a8e9ba5ee142
Rubén Díaz <89550712+rubdh@users.noreply.github.com>
2021-11-12 01:52:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
+    window.onload = function (){
+        let nombre = window.prompt("¡Hola! Introduce tu nombre, por favor :)");
+        document.getElementById("bienvenida").innerHTML = '¡Hola ' + nombre + '!';
 
-function ai(){
-    document.getElementById("respuesta").innerHTML = document.getElementById("pregunta").value;
-}
+        if(nombre == null){
+            document.getElementById("bienvenida").innerHTML = '¡Hola ' + 'quien seas :)';
+        }
+    }
+
+   function ai(){
+       const pregunta = document.getElementById("pregunta").value;
+
+        if (pregunta.indexOf("obsesión")){
+           document.getElementById("respuesta").innerHTML = obsesion;
+       }
+
+       if (pregunta.indexOf("toc")){
+           document.getElementById("respuesta").innerHTML = toc;
+       }
+
+       if (pregunta.indexOf("obsesión")){
+           document.getElementById("respuesta").innerHTML = tocNoDuele;
+       }
+    }
