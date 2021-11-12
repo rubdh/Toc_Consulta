f4b20df93dac93d17077ed4ce32575975a6e86b1
Rubén Díaz <89550712+rubdh@users.noreply.github.com>
2021-11-12 12:22:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+    // Author: @rubdh
+    // Cuando la página cargue, obtenemos el nombre del usuario y lo usamos para saludarle :)
     window.onload = function (){
         let nombre = window.prompt("¡Hola! Introduce tu nombre, por favor :)");
         document.getElementById("bienvenida").innerHTML = '¡Hola ' + nombre + '!';
@@ -7,6 +9,7 @@
         }
     }
 
+    // Creamos la función que evalúa la pregunta y según la palabra que contenga, obtendrá una respuesta u otra.
    function ai(){
        const pregunta = document.getElementById("pregunta").value;
 
