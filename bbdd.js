8657a8297772aae1bfcfa04d987983578a92d915
Rubén Díaz <89550712+rubdh@users.noreply.github.com>
2021-11-12 01:53:39 +0100

    Add files via upload

---
+++ bbdd.js
@@ -0,0 +1,4 @@
+
+    const obsesion = "Una obsesión es un conjunto de argumentos lógicos o pensamientos que construyen una realidad concreta.";
+    const toc = "El TOC o Trastorno Obsesivo Compulsivo es un trastorno que se basa en obsesiones, rituales, pensamientos intrusivos y el propio diálogo interno.";
+    const tocNoDuele = "No, el TOC no causa dolencia física.";
